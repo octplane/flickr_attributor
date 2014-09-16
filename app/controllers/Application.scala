74baad3001a63a5483c57a5c8493af46cd90e1a4
Pierre Baillet <pierre@baillet.name>
2014-09-17 00:52:58 +0200

    Licence Icon

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -41,7 +41,9 @@ object Application extends Controller {
     val img = new FlickrImage(id)
     val title = img.title
     val user = img.user
-    val l = img.license.toString
+    val license = new License(img.license.toString)
+
+    println(license.icon)
 
     val src = img.images("Medium 640")
     val reader = ImageIO.getImageReadersBySuffix("jpg").next();
@@ -60,22 +62,23 @@ object Application extends Controller {
     val w = sourceBuffer.getWidth
     val h = sourceBuffer.getHeight
     val target = new BufferedImage(
-      w, h+20, sourceBuffer.getType)
+      w, h+21, sourceBuffer.getType)
 
     val g2d = target.createGraphics()
 
-    g2d.setRenderingHint(
-      RenderingHints.KEY_ANTIALIASING,
-      RenderingHints.VALUE_ANTIALIAS_ON)
+    // g2d.setRenderingHint(
+    //   RenderingHints.KEY_TEXT_ANTIALIASING,
+    //   RenderingHints.VALUE_TEXT_ANTIALIAS_GASP)
 
     g2d.drawImage(sourceBuffer, 0, 0, null)
+    g2d.drawImage(license.iconBufferedImage, 1, sourceBuffer.getHeight + 3, null )
     g2d.setPaint(Color.lightGray)
-    g2d.setFont(new Font("Serif", Font.PLAIN, 19))
+    g2d.setFont(new Font("Serif", Font.PLAIN, 14))
 
     val text = s"$title by $user on Flickr"
     val fm = g2d.getFontMetrics()
-    val x = 1
-    val y = sourceBuffer.getHeight + 20 - fm.getMaxDescent
+    val x = 90
+    val y = sourceBuffer.getHeight + 18 - fm.getMaxDescent
     g2d.drawString(text, x, y)
     g2d.dispose();
 
@@ -137,6 +140,52 @@ class FlickrImage(id: String) {
       (sz \ "label").asOpt[JsString].get.value -> (sz \ "source").asOpt[JsString].get.value
     ).toMap
   }
+
+
+}
+
+class License(id: String) {
+  val alias = {
+    id match {
+      case "1" => "by-nc-sa"
+      case "2" => "by-nc"
+      case "3" => "by-nc-nd"
+      case "4" => "by"
+      case "5" => "by-sa"
+      case "6" => "by-nd"
+    }
+  }
+
+  val icon = {
+    val lic = alias
+    s"http://i.creativecommons.org/l/$lic/2.0/80x15.png"
+  }
+
+  // <license id="0" name="All Rights Reserved" url="" />
+  // <license id="1" name="Attribution-NonCommercial-ShareAlike License" url="http://creativecommons.org/licenses/by-nc-sa/2.0/" />
+  // <license id="2" name="Attribution-NonCommercial License" url="http://creativecommons.org/licenses/by-nc/2.0/" />
+  // <license id="3" name="Attribution-NonCommercial-NoDerivs License" url="http://creativecommons.org/licenses/by-nc-nd/2.0/" />
+  // <license id="4" name="Attribution License" url="http://creativecommons.org/licenses/by/2.0/" />
+  // <license id="5" name="Attribution-ShareAlike License" url="http://creativecommons.org/licenses/by-sa/2.0/" />
+  // <license id="6" name="Attribution-NoDerivs License" url="http://creativecommons.org/licenses/by-nd/2.0/" />
+  // <license id="7" name="No known copyright restrictions" url="http://flickr.com/commons/usage/" />
+  // <license id="8" name="United States Government Work" url="http://www.usa.gov/copyright.shtml" />
+
+  val iconBufferedImage = {
+    val reader = ImageIO.getImageReadersBySuffix("png").next
+
+    // Fix me: Colors are distorted...
+    val image = Http(icon)
+      .option(HttpOptions.connTimeout(2000))
+      .option(HttpOptions.readTimeout(5000)) { inputStream =>
+        val imageInputStream = ImageIO.createImageInputStream(inputStream)
+        reader.setInput(imageInputStream);
+        reader.readAll(0, null); // Important, also read metadata
+    }
+
+    image.getRenderedImage.asInstanceOf[java.awt.image.BufferedImage]
+  }
+
 
 
 }
