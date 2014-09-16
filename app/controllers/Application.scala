7eeda13fe683ef9526d0372d15321de55b0a19c6
Pierre Baillet <pierre@baillet.name>
2014-09-16 16:29:37 +0200

    Fix metadata copy

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,15 @@ import play.api._
 import play.api.mvc._
 
 import scalaj.http.Http
+import scalaj.http.HttpOptions
+
 
 import java.io.InputStreamReader
 import play.api.libs.json._
+
+import javax.imageio._
+import java.io.ByteArrayOutputStream
+import javax.imageio.ImageWriteParam._
 
 object Application extends Controller {
 
@@ -26,9 +32,33 @@ object Application extends Controller {
     val t = img.title
     val l = img.license.toString
 
-    // Ok(views.html.index(s"$t $l"))
-    Ok(img.images("Medium 640"))
-  }
+    val src = img.images("Medium 640")
+    val reader = ImageIO.getImageReadersBySuffix("jpg").next();
+
+    // Fix me: Colors are distorted...
+    val i = Http(src)
+      .option(HttpOptions.connTimeout(2000))
+      .option(HttpOptions.readTimeout(5000)) { inputStream =>
+        val imageInputStream = ImageIO.createImageInputStream(inputStream);
+        reader.setInput(imageInputStream);
+        reader.readAll(0, null); // Important, also read metadata
+    }
+
+    val renderedImage = i.getRenderedImage();
+
+    // Modify renderedImage as you like
+
+    val writer = ImageIO.getImageWriter(reader);
+    val param = writer.getDefaultWriteParam();
+    param.setCompressionMode(MODE_COPY_FROM_METADATA); // This modes ensures closest to original compression
+
+    val bo = new ByteArrayOutputStream
+    val ios = ImageIO.createImageOutputStream(bo)
+    writer.setOutput(ios)
+    writer.write(null, i, param)
+
+
+    Ok(bo.toByteArray).as("image/jpeg")  }
 
 }
 
@@ -42,22 +72,26 @@ class FlickrImage(id: String) {
   val rawLicense = (__ \ 'photo \ 'license ).json.pick[JsString]
 
 
+  val baseRequest = Http(flickREndpoint)
+      .param("api_key", key)
+      .param("nojsoncallback", "1")
+      .param("format", "json")
+      .option(HttpOptions.connTimeout(2000))
+      .option(HttpOptions.readTimeout(5000))
 
   lazy val info = {
-    val src = Http(flickREndpoint).param("method","flickr.photos.getInfo")
-      .param("api_key", key)
-      .param("photo_id", id)
-      .param("nojsoncallback", "1")
-      .param("format", "json").asString
+    val src = baseRequest
+      .param("method","flickr.photos.getInfo")
+      .param("photo_id", id).asString
+
     Json.parse(src)
   }
 
   lazy val sizes = {
-    val src = Http(flickREndpoint).param("method","flickr.photos.getSizes")
-      .param("api_key", key)
-      .param("photo_id", id)
-      .param("nojsoncallback", "1")
-      .param("format", "json").asString
+    val src = baseRequest
+      .param("method","flickr.photos.getSizes")
+      .param("photo_id", id).asString
+
     Json.parse(src)
   }
 
